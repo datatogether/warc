f9ce4728c23397e0c5887650be1cc9ffca887626
Brendan O'Brien <sparkle_pony_2000@qri.io>
2017-10-25 02:19:53 -0400

    adding in header rewrite methods

--- rewrite/header.go
+++ rewrite/header.go
@@ -13,9 +13,11 @@ const (
 )
 
 type HeaderRewriter struct {
-	prefix string
-	rules  map[string]RewriteRule
-	Urlrw  Rewriter
+	prefix           string
+	rules            map[string]RewriteRule
+	Urlrw            Rewriter
+	Cookierw         Rewriter
+	RewritingContent bool
 }
 
 func NewHeaderRewriter(configs ...func(cfg *Config)) *HeaderRewriter {
@@ -24,6 +26,8 @@ func NewHeaderRewriter(configs ...func(cfg *Config)) *HeaderRewriter {
 		prefix: c.HeaderPrefix,
 		rules:  c.HeaderRules,
 		Urlrw:  c.Defmod,
+		// Cookierw: c.CookieRewriter,
+		// RewritingContent: c.ContentRewriter != nil,
 	}
 }
 
@@ -31,62 +35,43 @@ func (hrw *HeaderRewriter) Rewrite(p []byte) ([]byte, error) {
 	return nil, ErrNotFinished
 }
 
-func (hrw *HeaderRewriter) rewriteHeader(name, value string) (string, string) {
+func (hrw *HeaderRewriter) RewriteHeader(name, value string) (string, string) {
 	switch hrw.rules[name] {
 	case Keep:
 		return name, value
 	case UrlRewrite:
-
+		if hrw.Urlrw != nil {
+			return name, string(hrw.Urlrw.Rewrite([]byte(value)))
+		}
+		return name, value
 	case PrefixIfContentRewrite:
+		if hrw.RewritingContent {
+			return hrw.prefix + name, value
+		}
+		return name, value
 	case PrefixIfUrlRewrite:
+		if hrw.Urlrw != nil {
+			return hrw.prefix + name, value
+		}
+		return name, value
 	case ContentLength:
-	case Cookie:
-	case Prefix:
-
-		// if rule == 'keep':
-		//           return (name, value)
-
-		//       elif rule == 'url-rewrite':
-		//           if self.rwinfo.is_url_rw():
-		//               return (name, self.rwinfo.url_rewriter.rewrite(value))
-		//           else:
-		//               return (name, value)
-
-		//       elif rule == 'prefix-if-content-rewrite':
-		//           if self.rwinfo.is_content_rw:
-		//               return (self.header_prefix + name, value)
-		//           else:
-		//               return (name, value)
-
-		//       elif rule == 'prefix-if-url-rewrite':
-		//           if self.rwinfo.is_url_rw():
-		//               return (self.header_prefix + name, value)
-		//           else:
-		//               return (name, value)
-
-		//       elif rule == 'content-length':
-		//           if value == '0':
-		//               return (name, value)
-
+		if value == "0" {
+			return name, value
+		}
 		//           if not self.rwinfo.is_content_rw:
 		//               try:
 		//                   if int(value) >= 0:
 		//                       return (name, value)
 		//               except:
 		//                   pass
-
-		//           return (self.header_prefix + name, value)
-
-		//       elif rule == 'cookie':
-		//           if self.rwinfo.cookie_rewriter:
-		//               return self.rwinfo.cookie_rewriter.rewrite(value)
-		//           else:
-		//               return (name, value)
-
-		//       elif rule == 'prefix':
-		//           return (self.header_prefix + name, value)
-
-		//       return (name, value)
+		return hrw.prefix + name, value
+	case Cookie:
+		if hrw.Cookierw != nil {
+			//               return self.rwinfo.cookie_rewriter.rewrite(value)
+		}
+		return name, value
+	case Prefix:
+		return hrw.prefix + name, value
 	}
 	return name, value
 }
